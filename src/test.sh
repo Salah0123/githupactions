1c5efef7852abff1976aa3bae38817457386b27c
Salah0123 <116020741+Salah0123@users.noreply.github.com>
2025-12-01 11:28:09 +0300

    modify

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 EXPECTED="Hello, Test!"
-OUTPUT=${node -e "console.log(require('./src/app')('Test'))"}
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 if [ "$OUTPUT" == "$EXPECTED" ]; then
 echo "Test passed!"
 exit 0
